6904b6a8162049d3d6b6531d51683b8914452695
harimohan1990 <harimohan.info@gmail.com>
2020-10-12 18:53:46 +0530

    accordian html css javascript

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-// linear search -
-//loop through each element of the array and return the match
-// called linear search 
-// want to find x = 50 ,return the position 
-
-//  arr = [10,20, 30 ,50,60];
-// output - 4 , x = 50
-//output -80 , return -1 
-// where,  x is element to find 
-
-//[10,20,40,50,50],50)
-
-function linearSearch(arr, x){
-    for(let i=0; i<arr.length;i++){
-        if(arr[i] ==x){
-            return i
-        }
-    }
-    return null
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
 }
-console.log(linearSearch([10,20,40,50,50],50))
-
-
-
-
-
-
-
