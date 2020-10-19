6901523eadc4e863ac36eefdc0eb4cd9624a93a7
harimohan1990 <harimohan.info@gmail.com>
2020-10-19 18:53:30 +0530

    commit

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
+// reducer 
+
+function counter(state, action){
+  if(typeof state==="undefined"){
+    return 0
+  }
+  switch(action.type){
+    case "INCREMENT":
+      return state+1
+    case "DECREMENT":
+      return state-1
+    default: 
+    return state
+  }
 }
+
+// creating store here 
+var store = Redux.createStore(counter)
+
+var ValueE1 = document.getElementById('value');
+
+function render(){
+ValueE1.innerHTML = store.getState().toString()
+}
+render()
+store.subscribe(render)
+
+document.getElementById("increment")
+.addEventListener("click", function(){
+  store.dispatch({type:"INCREMENT"})
+})
+
+document.getElementById("decrement")
+.addEventListener("click", function(){
+  store.dispatch({type:"DECREMENT"})
+})
+
+document.getElementById("incrementIfOdd")
+.addEventListener('click',function(){
+  if(store.getState() % 2 !==0){
+    store.dispatch({type:"DECREMENT"})
+  }
+})
+
+document.getElementById("incrementAsync")
+.addEventListener('click',function(){
+  setTimeout(function(){
+    store.dispatch({type:'DECREMENT'})
+  }, 1000)
+})
